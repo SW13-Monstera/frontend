75ed5b0edf6df21c597389a7ee91dd147cb481c3
kimhyunjo <alsxmqkqh23@gmail.com>
2022-07-02 17:37:59 +0900

    ✨ Add Slider
    - 사실 개발은 안함

---
+++ src/Component/Utils/Slider.tsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+
+function Slider() {
+  return <div css={sliderWrapperStyle}>Slider</div>;
+}
+
+const sliderWrapperStyle = css`
+  width: 100%;
+  height: 355.61px;
+  background: #000000;
+`;
+
+export default Slider;
