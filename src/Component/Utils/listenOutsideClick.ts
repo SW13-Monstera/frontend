0312a4cd635b385d2360d740d875bbbd5c9781c8
kimhyunjo <alsxmqkqh23@gmail.com>
2022-07-04 14:13:36 +0900

    ✨ Add listenOutsideClick function
    - menuRef가 가르키는 dom요소(드롭다운)가 아닌 곳이 클릭되면 isOpen false로 바꾸기

---
+++ src/Component/Utils/listenOutsideClick.ts
@@ -0,0 +1,19 @@
+import { RefObject } from 'react';
+
+export default function listenOutsideClick( //menuRef가 가르키는 dom요소(드롭다운)가 아닌 곳이 클릭되면 isOpen false로
+  menuRef: RefObject<HTMLDivElement>,
+  setIsOpen: (isOpen: boolean) => void,
+) {
+  return () => {
+    if (!menuRef.current) return;
+
+    ['click', 'touchstart'].forEach((e) => {
+      document.addEventListener(e, (event) => {
+        const curr = menuRef.current;
+        const target = event.target;
+        if (curr?.contains(target as Node)) return;
+        setIsOpen(false);
+      });
+    });
+  };
+}
