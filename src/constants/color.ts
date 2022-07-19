351e8e4c86f17e23ac9869bd89623a2d1e7e8380
kimhyunjo <alsxmqkqh23@gmail.com>
2022-07-19 10:51:47 +0900

    🧱 Define Color Constants

---
+++ src/constants/color.ts
@@ -0,0 +1,11 @@
+const COLOR = {
+  primary: '#4682DB', //서비스 서브 컬러
+  highlight: '#FFF1CD', // 하이라이트 컬러
+  error: '#FF6D6D', // 오류 및 삭제에 사용
+  titleActive: '#000000', // 각종 타이틀 및 텍스트에 사용
+  gray1: '#D9D9D9', // border, placeholder에 사용
+  white1: '#FFFFFF', // 기본 배경색, 텍스트 등에 사용되는 흰색
+  offWhite: '#F5F5F5', // 서브 배경색
+};
+
+export { COLOR };
