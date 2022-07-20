fe1902a030632f72e15e68e1e3b93409a352da1c
kimhyunjo <alsxmqkqh23@gmail.com>
2022-07-20 17:36:48 +0900

    💄Add padding in login inputbox

--- src/Component/Box/InputBox/DefaultInputBox/style.css.ts
+++ src/Component/Box/InputBox/DefaultInputBox/style.css.ts
@@ -10,6 +10,9 @@ export const [inputBoxClass, inputVars] = createTheme({
 });
 
 export const defaultInputBoxStyle = style({
+  'display': 'flex',
+  'alignItems': 'center',
+
   'color': '#000000',
   'background': '#FFFFFF',
   'border': '1px solid #D9D9D9',
@@ -21,8 +24,8 @@ export const defaultInputBoxStyle = style({
   'fontWeight': '300',
   'fontSize': '2rem',
   'lineHeight': '2.4375rem',
-  'display': 'flex',
-  'alignItems': 'center',
+
+  'padding': ' 0 1rem 0 1rem',
 
   '::placeholder': {
     fontFamily: 'Inter',
