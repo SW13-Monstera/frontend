afeb260182cc1ac8f04dbe8ad5bcb53b38e2c522
kimhyunjo <alsxmqkqh23@gmail.com>
2022-07-20 20:59:47 +0900

    💄 Change oauth logo file format
    - from png to svg

--- src/Organism/ButtonList/OAuthButtonList/index.tsx
+++ src/Organism/ButtonList/OAuthButtonList/index.tsx
@@ -1,16 +1,16 @@
 import OAuthButton, { OAUTH } from '../../../Component/Button/OAuthButton';
-import githubLogo from '../../../assets/images/github.png';
-import googleLogo from '../../../assets/images/google.png';
-import { oauthButtonListStyle, oAuthImgStyle } from './style.css';
+import githubLogo from '../../../assets/icons/github.svg';
+import googleLogo from '../../../assets/icons/google.svg';
+import { oauthButtonListStyle, oAuthGithubImgStyle, oAuthGoogleImgStyle } from './style.css';
 
 function OAuthButtonList() {
   return (
     <div className={oauthButtonListStyle}>
       <OAuthButton oAuth={OAUTH.GITHUB}>
-        <img src={githubLogo} className={oAuthImgStyle} />
+        <img src={githubLogo} alt='github logo' className={oAuthGithubImgStyle} />
       </OAuthButton>
       <OAuthButton oAuth={OAUTH.GOOGLE}>
-        <img src={googleLogo} className={oAuthImgStyle} />
+        <img src={googleLogo} alt='google logo' className={oAuthGoogleImgStyle} />
       </OAuthButton>
     </div>
   );
