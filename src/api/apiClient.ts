7051350c9972593157b1449ce227f176d9b1cfa9
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-08-09 19:43:32 +0900

    🧱 Add mode
    - dev, prod

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,23 +2,9 @@ import axios from 'axios';
 import { authApiWrapper } from './wrapper/auth/authApiWrapper';
 
 const apiClient = axios.create({
-  baseURL: 'https://dev.api.csbroker.io/api',
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: true,
 });
-
-// isLogin? -> localstorage userInfo check -> 아래 로직 세팅
-try {
-  const userInfoString = localStorage.getItem('userInfo');
-  if (userInfoString !== null) {
-    const userInfo = JSON.parse(userInfoString);
-    const token: string | null | undefined = userInfo.accessToken;
-    if (typeof token === 'string') {
-      apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    }
-  }
-} catch (e) {
-  ('');
-}
 
 apiClient.interceptors.response.use(
   (res) => res.data,
