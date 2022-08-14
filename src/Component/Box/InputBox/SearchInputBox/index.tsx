782c8a11128f0f0a6ce13dc7903fbd0c1926b7a8
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-08-14 19:35:31 +0900

    ✨ Search when enter keydown event occurs

--- src/Component/Box/InputBox/SearchInputBox/index.tsx
+++ src/Component/Box/InputBox/SearchInputBox/index.tsx
@@ -3,12 +3,19 @@ import { INPUT_TYPE } from '../../../../constants/input';
 import { BUTTON_THEME, BUTTON_TYPE } from '../../../../types/button';
 import IconButton from '../../../Button/IconButton';
 import { searchInputBoxStyle, inputTextBoxStyle, searchButtonStyle } from './style.css';
+import { KeyboardEvent } from 'react';
 
 interface ISearchInputBox {
   handleSearchInput: () => void;
 }
 
 function SearchInputBox({ handleSearchInput }: ISearchInputBox) {
+  function onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleSearchInput();
+    }
+  }
   return (
     <form className={searchInputBoxStyle}>
       <label htmlFor='search-problem'></label>
@@ -17,6 +24,7 @@ function SearchInputBox({ handleSearchInput }: ISearchInputBox) {
         className={inputTextBoxStyle}
         type={INPUT_TYPE.SEARCH}
         id='search-problem'
+        onKeyDown={onKeyDown}
       ></input>
       <IconButton
         type={BUTTON_TYPE.BUTTON}
