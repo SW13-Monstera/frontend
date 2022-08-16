51af9be60dbede244d1213b37f746762faeaae1d
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-08-17 01:20:56 +0900

    🏷 Fix var name

--- src/utils/userInfo.ts
+++ src/utils/userInfo.ts
@@ -1,15 +1,15 @@
 import { USER_INFO } from '../constants/localStorage';
 import { IUserInfo } from '../types/auth';
 
-export const setUserInfo = (json: IUserInfo) => {
-  const userInfoString = JSON.stringify(json);
+export const setUserInfo = (userInfo: IUserInfo) => {
+  const userInfoString = JSON.stringify(userInfo);
   localStorage.setItem(USER_INFO, userInfoString);
 };
 
 export const getUserInfo = (): IUserInfo | undefined => {
-  const userInfo = localStorage.getItem(USER_INFO);
-  if (!userInfo) return undefined;
-  return JSON.parse(userInfo);
+  const userInfoString = localStorage.getItem(USER_INFO);
+  if (!userInfoString) return undefined;
+  return JSON.parse(userInfoString);
 };
 
 export const removeUserInfo = () => {
