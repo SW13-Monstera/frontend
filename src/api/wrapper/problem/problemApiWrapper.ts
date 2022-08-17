80772d58c04c936bb36ec4013449916c9b4bb055
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-08-17 16:00:16 +0900

    🐛 Updateproblem list api request config
    - when isSolved included

--- src/api/wrapper/problem/problemApiWrapper.ts
+++ src/api/wrapper/problem/problemApiWrapper.ts
@@ -1,10 +1,21 @@
+import { getUserInfo } from './../../../utils/userInfo';
 import apiClient from '../../apiClient';
 import { API_URL, API_URL_WITH_PARAMS } from '../../../constants/apiUrl';
 import { IProblemRequestParam } from '../../../types/api/problem';
+import { BEARER_TOKEN } from '../../../constants/api';
+import { AxiosRequestConfig } from 'axios';
 
 export const problemApiWrapper = {
   problemList: (params: IProblemRequestParam) => {
-    return apiClient.get(API_URL.PROBLEM_LIST, { params: params });
+    const config: AxiosRequestConfig<any> = {
+      params: params,
+    };
+    if (params.isSolved) {
+      const userInfo = getUserInfo();
+      if (!userInfo) throw new Error('userinfo not found');
+      config.headers = { Authorization: BEARER_TOKEN(userInfo.accessToken) };
+    }
+    return apiClient.get(API_URL.PROBLEM_LIST, config);
   },
   problemDetail: (problem_id: string) => {
     return apiClient.get(API_URL_WITH_PARAMS.PROBLEM_DETAIL(problem_id));
