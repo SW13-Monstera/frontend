e13d62721fe7c863804e48a991e4682569240f29
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-08-17 12:04:15 +0900

    🥅 Catch errors in JSON stringify/parse

--- src/utils/userInfo.ts
+++ src/utils/userInfo.ts
@@ -1,15 +1,43 @@
 import { USER_INFO } from '../constants/localStorage';
 import { IUserInfo } from '../types/auth';
 
-export const setUserInfo = (userInfo: IUserInfo) => {
-  const userInfoString = JSON.stringify(userInfo);
-  localStorage.setItem(USER_INFO, userInfoString);
+export const isValidJSONString = (str: object) => {
+  try {
+    JSON.stringify(str);
+    return true;
+  } catch {
+    return false;
+  }
 };
 
-export const getUserInfo = (): IUserInfo | undefined => {
+export const isValidJSON = (text: string) => {
+  try {
+    JSON.parse(text);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const setUserInfo = (userInfo: IUserInfo) => {
+  if (isValidJSONString(userInfo)) {
+    const userInfoString = JSON.stringify(userInfo);
+    localStorage.setItem(USER_INFO, userInfoString);
+  } else {
+    throw new Error('invalid json string format');
+  }
+};
+
+export const getUserInfo = (): IUserInfo => {
   const userInfoString = localStorage.getItem(USER_INFO);
-  if (!userInfoString) return undefined;
-  return JSON.parse(userInfoString);
+  if (!userInfoString) throw new Error('userinfo not found');
+
+  if (isValidJSON(userInfoString)) {
+    const userInfo = JSON.parse(userInfoString);
+    return userInfo;
+  } else {
+    throw new Error('invalid json format');
+  }
 };
 
 export const removeUserInfo = () => {
