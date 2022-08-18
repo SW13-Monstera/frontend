ec795b560bfaa3d54a200ab701044c98f1f52d3f
kimhyunjo <alsxmqkqh23@gmail.com>
2022-08-18 15:08:06 +0900

    ♻️ Change function name
    - setUserInfo -> setUserData

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -6,13 +6,25 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
+// isLogin? -> localstorage userInfo check -> 아래 로직 세팅
+try {
+  const userInfoString = localStorage.getItem('userInfo');
+  if (userInfoString !== null) {
+    const userInfo = JSON.parse(userInfoString);
+    const token: string | null | undefined = userInfo.accessToken;
+    if (typeof token === 'string') {
+      apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
+  }
+} catch (e) {}
+
 apiClient.interceptors.response.use(
   (res) => res.data,
   (err) => {
     const { status } = err.response;
 
     if (status === 401) {
-      authApiWrapper.refresh();
+      // authApiWrapper.refresh();
     } else if (status === 400 || status === 500) {
       //   location.reload();
     } else if (status !== 200) {
