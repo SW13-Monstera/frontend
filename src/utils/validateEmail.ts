e0c2839e696b9eb723976f0aefc576829f5cc4b2
kimhyunjo <alsxmqkqh23@gmail.com>
2022-08-18 19:05:40 +0900

    🚨 Fix lint error

--- src/utils/validateEmail.ts
+++ src/utils/validateEmail.ts
@@ -1,4 +1,4 @@
 export const validateEmail = (emailValue: string) => {
-  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\w{2,3})+$/;
   return re.test(emailValue);
 };
