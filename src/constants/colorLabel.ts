88ee04ff92a3a87891eeedffc062004c14891ab9
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-09-02 02:27:11 +0900

    🧱 Update constants, mock

---
+++ src/constants/colorLabel.ts
@@ -0,0 +1,7 @@
+import { COLOR } from './color';
+
+export const COLOR_LABEL_LIST = [
+  { color: COLOR.POINT1, name: '서술형', type: 'long' },
+  { color: COLOR.POINT2, name: '단답형', type: 'short' },
+  { color: COLOR.POINT3, name: '객관식', type: 'multiple' },
+];
