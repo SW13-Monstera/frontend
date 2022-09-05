8881663a6b786cdb42b9014330441c91090a71ab
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-09-05 09:44:02 +0900

    🧱 Add constants
    - 유저 정보 수정 페이지에 필요
    - 추후 서버에서 받을 것도 있음

---
+++ src/constants/userDataEdit.ts
@@ -0,0 +1,21 @@
+export const JOB = [
+  { id: 'under-high-school', name: '고등학생 이하' },
+  { id: 'undergraduate', name: '대학생' },
+  { id: 'graduate', name: '대학원생' },
+  { id: 'developer', name: '직장인(개발자)' },
+  { id: 'not-developer', name: '직장인(비개발자)' },
+  { id: 'independent', name: '무소속(취업준비생)' },
+  { id: 'etc', name: '기타' },
+];
+
+export const CORE_TECH = [
+  { id: 'reactJS', name: 'ReactJS' },
+  { id: 'nodeJS', name: 'NodeJS' },
+  { id: 'java', name: 'Java' },
+];
+
+export const JOB_OBJECTIVE = [
+  { id: 'BACKEND', name: '서버/백엔드' },
+  { id: 'FRONTEND', name: '프론트엔드' },
+  { id: 'AI', name: '인공지능' },
+];
