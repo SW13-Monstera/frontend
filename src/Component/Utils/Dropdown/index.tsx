a4c625272b6e786dd145d47ea3fcd332ed802671
kimhyunjo <alsxmqkqh23@gmail.com>
2022-09-27 17:18:42 +0900

    💄 Update Problem list page UI

--- src/Component/Utils/Dropdown/index.tsx
+++ src/Component/Utils/Dropdown/index.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
-import { ReactComponent as DownIcon } from '../../../assets/icons/down-arrow-icon.svg';
-import { ReactComponent as UpIcon } from '../../../assets/icons/up-arrow-icon.svg';
 import { useCheckedTagStore } from '../../../hooks/useStore';
+import { DownArrowIcon } from '../../../Icon/DownArrowIcon';
+import { UpArrowIcon } from '../../../Icon/UpArrowIcon';
+import { themeColors } from '../../../styles/theme.css';
 import { BUTTON_TYPE } from '../../../types/button';
 import { IDropdownElement } from '../../../types/util';
 import listenOutsideClick from '../../../utils/listenOutsideClick';
 import { DropdownElement } from '../DropdownElement';
 import {
-  downIconStyle,
   dropdownBoxStyle,
   dropdownContentVisibilityStyle,
   dropDownListStyle,
@@ -38,7 +38,11 @@ function Dropdown({ name, elements, handleCheckedTags }: IDropdownProps) {
     <div className={dropDownStyle} ref={menuRef}>
       <button className={dropdownBoxStyle} onClick={onClickDropdown} type={BUTTON_TYPE.BUTTON}>
         <p>{name} 선택</p>
-        {isOpen ? <UpIcon className={downIconStyle} /> : <DownIcon className={downIconStyle} />}
+        {isOpen ? (
+          <UpArrowIcon width='.9rem' height='.6rem' fill={themeColors.text[3]} />
+        ) : (
+          <DownArrowIcon width='.9rem' height='.6rem' fill={themeColors.text[3]} />
+        )}
       </button>
       <div className={dropdownContentVisibilityStyle[isOpen ? 'visible' : 'hidden']}>
         <ul className={dropDownListStyle}>
