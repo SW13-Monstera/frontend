af49960ef4c74da1d6b2fec9ed26701ab90f8d09
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-11-03 18:54:42 +0900

    ✨ Add toast when error

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
   return (
     <div className={`App ${isDark ? darkTheme : lightTheme} ${appStyle}`}>
       <Helmet />
-      <ToastContainer />
+      <ToastContainer limit={3} />
       <Router />
     </div>
   );
--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { Navigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { API_BASE_URL, AUTHORIZTION, BEARER_TOKEN } from '../constants/api';
 import { URL } from '../constants/url';
+import { setLogout } from '../utils/setLogout';
 import { getUserInfo } from '../utils/userInfo';
 
 const apiClient = axios.create({
@@ -23,6 +25,15 @@ apiClient.interceptors.response.use(
     const { status } = err.response;
     if (status === 404) {
       Navigate({ to: URL.PAGE_NOT_FOUND });
+    } else if (status === 401) {
+      toast('로그인 후 이용가능해요😊');
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
+
+      setLogout();
+    } else if (status === 500) {
+      toast.error('앗! 오류가 발생했어요🥲' + '\n' + '잠시 후에 다시 시도해보세요.');
     }
   },
 );
