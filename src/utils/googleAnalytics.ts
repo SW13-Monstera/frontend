0f2575b027faa33554349bc5f34ea5010e620bb1
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-11-05 02:31:49 +0900

    🚑 Fix ga tracking id
    - ga id가 src에 직접 들어있는걸 못봤습니다... 빠르게 고쳐봅니다...

--- src/utils/googleAnalytics.ts
+++ src/utils/googleAnalytics.ts
@@ -3,7 +3,7 @@ import { GA_TRACKING_ID } from '../constants/api';
 export const addGoogleAnalyticsTag = () => {
   const googleAnalyticsTagElement = document.createElement('div');
   googleAnalyticsTagElement.innerHTML = `
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5YDJ48WT3K"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
