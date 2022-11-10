1e06d5d6cc8bbe6008cf85a8a713f04b396d8d54
kimhyunjo <alsxmqkqh23@gmail.com>
2022-11-10 16:46:06 +0900

    :sparkles: Add radio button

---
+++ src/Component/Input/RadioButton/index.tsx
@@ -0,0 +1,17 @@
+import { INPUT_TYPE } from '../../../constants/input';
+import './style.css';
+
+interface IRadioButton {
+  id: string;
+  label: string;
+  name: string;
+}
+
+export const RadioButton = ({ id, label, name }: IRadioButton) => {
+  return (
+    <label htmlFor={id} className='radioButtonWrapperStyle'>
+      <input type={INPUT_TYPE.RADIO} id={id} name={name} className='radioButtonStyle' />
+      <span className='labelTextStyle'>{label}</span>
+    </label>
+  );
+};
