103352810f5981e660c7bf924da25a1dff22856c
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-11-11 15:13:50 +0900

    :fire: Remove unused problem set code

--- src/Page/ProblemSet/ProblemSetDetail/LongResult/style.css.ts
+++
@@ -1,5 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const chartContentWrapperStyle = style({
-  padding: '1rem',
-});
--- src/Page/ProblemSet/ProblemSetDetail/Short/style.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const inputStyle = style({
-  display: 'flex',
-  flexDirection: 'column',
-  width: '100%',
-});
