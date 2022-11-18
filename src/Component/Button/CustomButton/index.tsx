75a8d68fa7fee7ef3d38b9d2cde08228165bde1e
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-11-18 15:33:31 +0900

    :sparkles: Add tag in custom button
    베타 서비스임을 알리기 위함

--- src/Component/Button/CustomButton/index.tsx
+++ src/Component/Button/CustomButton/index.tsx
@@ -1,9 +1,13 @@
 import { ICustomButton } from '../../../types/button';
+import { buttonTagStyle, customButtonWrapperStyle } from './style.css';
 
-export const CustomButton = ({ onClick, children, type, className }: ICustomButton) => {
+export const CustomButton = ({ onClick, children, type, className, tag }: ICustomButton) => {
   return (
-    <button type={type} onClick={onClick} className={className}>
-      {children}
-    </button>
+    <div className={customButtonWrapperStyle}>
+      {tag ? <div className={buttonTagStyle}>{tag}</div> : <></>}
+      <button type={type} onClick={onClick} className={`${className}`}>
+        {children}
+      </button>
+    </div>
   );
 };
