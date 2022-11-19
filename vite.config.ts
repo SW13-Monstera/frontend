5b92f42ce3cb04ba110d8de80a372ae5751d07c8
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-11-19 19:53:55 +0900

    :heavy_plus_sign: Add rollup-plugin-visualizer
    번들 사이즈 확인 위함

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import removeConsole from 'vite-plugin-remove-console';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [removeConsole(), svgr(), vanillaExtractPlugin(), react()],
+  plugins: [
+    removeConsole(),
+    svgr(),
+    vanillaExtractPlugin(),
+    react(),
+    visualizer({
+      open: true,
+    }),
+  ],
 });
