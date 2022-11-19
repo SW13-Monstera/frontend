284cef40c4a48f981ae6b7420841430b9f74a129
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-11-19 20:07:04 +0900

    :zap: Replace github icon to svg file in oauth

--- src/Organism/ButtonList/OAuthButtonList/index.tsx
+++ src/Organism/ButtonList/OAuthButtonList/index.tsx
@@ -1,6 +1,6 @@
 import OAuthButton, { OAUTH } from '../../../Component/Button/OAuthButton';
 import { oauthButtonListStyle } from './style.css';
-import { GithubIcon } from '../../../Icon/GithubIcon';
+import { ReactComponent as GithubIcon } from '../../../assets/icons/github.svg';
 import { ReactComponent as GoogleIcon } from '../../../assets/icons/google.svg';
 
 function OAuthButtonList() {
@@ -8,10 +8,14 @@ function OAuthButtonList() {
     <div className={oauthButtonListStyle}>
       <OAuthButton
         oAuth={OAUTH.GITHUB}
-        icon={<GithubIcon width='1.5rem' height='1.5rem' />}
+        icon={<GithubIcon width='25px' height='30px' />}
         text='깃허브로 로그인'
       />
-      <OAuthButton oAuth={OAUTH.GOOGLE} icon={<GoogleIcon />} text='구글로 로그인' />
+      <OAuthButton
+        oAuth={OAUTH.GOOGLE}
+        icon={<GoogleIcon width='20px' height='20px' />}
+        text='구글로 로그인'
+      />
     </div>
   );
 }
