8857654f87a45e1fba2e27ccc79501630a6ccef5
“Kim-Hyunjo” <alsxmqkqh23@gmail.com>
2022-11-23 02:51:56 +0900

    :fire: Remove 401 error code

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { API_BASE_URL, AUTHORIZTION, BEARER_TOKEN } from '../constants/api';
 import { URL } from '../constants/url';
-import { setLogout } from '../utils/setLogout';
 import { getUserInfo } from '../utils/userInfo';
 
 const apiClient = axios.create({
@@ -24,14 +23,6 @@ apiClient.interceptors.response.use(
     const { status } = err.response;
     if (status === 404) {
       location.replace(URL.PAGE_NOT_FOUND);
-    } else if (status === 401) {
-      toast('로그인 후 이용가능해요😊');
-      setTimeout(() => {
-        setLogout();
-        location.reload();
-      }, 1000);
-
-      setLogout();
     } else if (status === 500) {
       toast.error('앗! 오류가 발생했어요🥲' + '\n' + '잠시 후에 다시 시도해보세요.');
     }
