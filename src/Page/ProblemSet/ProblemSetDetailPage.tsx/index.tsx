dd9c25871256c8c1ee966d802bfd1fc95f178538
kimhyunjo <alsxmqkqh23@gmail.com>
2022-11-26 21:38:12 +0900

    :fire: Remove dev server related codes

--- src/Page/ProblemSet/ProblemSetDetailPage.tsx/index.tsx
+++ src/Page/ProblemSet/ProblemSetDetailPage.tsx/index.tsx
@@ -6,7 +6,6 @@ import { PROBLEM_TYPE } from '../../../constants/problem';
 import { IProblemDetailResponseData } from '../../../types/api/problem';
 import { TPartialProblemDetailResponseData } from '../../../types/problem';
 import { IProblemSetDataElement, IProblemSetProblemsElement } from '../../../types/problemSet';
-import { isProduction } from '../../../utils/isProduction';
 import { ErrorPage } from '../../Error/ErrorPage';
 import { MetaTag } from '../../utils/MetaTag';
 import {
@@ -23,12 +22,8 @@ export const ProblemSetDetailPage = () => {
   const [problemList, setProblemList] = useState<TPartialProblemDetailResponseData[]>();
 
   useEffect(() => {
-    import(
-      isProduction ? '../../../mock/problemSet.json' : '../../../mock/problemSetDev.json'
-    ).then((data) => {
-      const json: IProblemSetDataElement[] = JSON.parse(JSON.stringify(data)).default;
-      setProblemSetData(json[parseInt(setId)]);
-    });
+    const json: IProblemSetDataElement[] = JSON.parse(JSON.stringify(problemSetData)).default;
+    setProblemSetData(json[parseInt(setId)]);
   }, []);
 
   useEffect(() => {
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,3 @@
-import { isProduction } from '../utils/isProduction';
-
 export const AUTHORIZTION = 'Authorization';
 export const BEARER_TOKEN = (token: string) => `Bearer ${token}`;
 export const ROLES = {
@@ -7,13 +5,8 @@ export const ROLES = {
   ROLE_USER: 'ROLE_USER',
 };
 
-export const API_BASE_URL = isProduction
-  ? 'https://api.csbroker.io'
-  : 'https://dev.api.csbroker.io';
+export const API_BASE_URL = 'https://api.csbroker.io';
 
-export const APP_URL = isProduction ? 'https://csbroker.io' : 'https://dev.csbroker.io';
+export const APP_URL = 'https://csbroker.io';
 
-export const GA_TRACKING_ID =
-  window.location.href.includes('dev') || window.location.href.includes('localhost')
-    ? 'UA-180471924-3'
-    : 'UA-180471924-2';
+export const GA_TRACKING_ID = 'UA-180471924-2';
