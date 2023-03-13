afd81c97a1ad17c77a7b1c53f5cf77d165c09c96
kimhyunjo <alsxmqkqh23@gmail.com>
2023-03-13 22:54:16 +0900

    :recycle: Refactor typewriter animation

--- src/utils/typewriter.ts
+++ src/utils/typewriter.ts
@@ -1,40 +1,21 @@
 import { RefObject } from 'react';
+
 export function typewriter(ref: RefObject<HTMLParagraphElement>, text: string | undefined) {
-  if (!text) return;
-
-  const textArr = new Array(text);
-  const iSpeed = 100;
-  let iIndex = 0;
-  let iArrLength = textArr[0].length;
-  const iScrollAt = 20;
-
-  let iTextPos = 0;
-  let sContents = '';
-  let iRow;
+  const speed = 100;
+  let curIdx = 0;
 
   function write() {
-    sContents = ' ';
-    iRow = Math.max(0, iIndex - iScrollAt);
+    if (!text || !ref.current) return;
 
-    while (iRow < iIndex) {
-      sContents += textArr[iRow++] + '<br />';
-    }
-    if (!ref.current) return;
-    ref.current.innerHTML = sContents + textArr[iIndex].substring(0, iTextPos) + '|';
-    if (iTextPos++ === iArrLength) {
-      iTextPos = 0;
-      iIndex++;
-      if (iIndex !== textArr.length) {
-        iArrLength = textArr[iIndex].length;
-        setTimeout(() => {
-          write();
-        }, 500);
-      }
+    ref.current.innerHTML = text.substring(0, curIdx) + '|';
+    if (curIdx++ === text.length) {
+      curIdx = 0;
     } else {
       setTimeout(() => {
         write();
-      }, iSpeed);
+      }, speed);
     }
   }
+
   write();
 }
